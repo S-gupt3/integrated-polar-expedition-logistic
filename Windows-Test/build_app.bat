6c8f5c0e5a20180c97c448b7772919f0af7c00b4
Versus-DEV <112648204+Versus-DEV@users.noreply.github.com>
2026-09-15 02:04:20 +0530

    Update build_app.bat for PyInstaller configuration

--- Windows-Test/build_app.bat
+++ Windows-Test/build_app.bat
@@ -18,6 +18,7 @@ if exist "PLOROPSIS.spec" del PLOROPSIS.spec
 :: Build
 pyinstaller --noconsole --name "PLOROPSIS" ^
     --add-data "test folder/frontend;test folder/frontend" ^
+    --add-data "test folder/frontend/assets/icons;test folder/frontend/assets/icons" ^
     --add-data "config.json;." ^
     --hidden-import=pymysql ^
     --hidden-import=uvicorn.logging ^
